122346e3e60c3ca2b3c3c6693c237ab708da77d5
mavrikkk <42514734+mavrikkk@users.noreply.github.com>
2021-02-19 12:55:30 +0300

    Add files via upload

---
+++ upmpdcli/rootfs/etc/cont-init.d/config.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# mpd and upmpdcli config
+# ==============================================================================
+
+set -e
+
+#setup folders
+bashio::log.info  'Setup Folders for MPD'
+
+mkdir -p /data/database \
+    /share/mpd/music \
+    /share/mpd/playlists
+	
+#setup configs
+bashio::log.info "Configuring upmpdcli..."
+
+friendlyname = "$(bashio::config 'friendlyname')"
+ohproductroom = "$(bashio::config 'ohproductroom')"
+spotifyuser = "$(bashio::config 'spotifyuser')"
+spotifypass = "$(bashio::config 'spotifypass')"
+
+sed "/#friendlyname/c friendlyname = $friendlyname" /etc/upmpdcli.conf
+sed "/#ohproductroom/c ohproductroom = $ohproductroom" /etc/upmpdcli.conf
+sed "/#spotifyuser/c spotifyuser = $spotifyuser" /etc/upmpdcli.conf
+sed "/#spotifypass/c spotifypass = $spotifypass" /etc/upmpdcli.conf
+
+#test logging
+cat /etc/upmpdcli.conf
