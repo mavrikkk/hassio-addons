eaaec224bbfcd0ac6d27be5b0619d0f6de2bb48f
mavrikkk <42514734+mavrikkk@users.noreply.github.com>
2021-02-19 15:40:39 +0300

    Update config.sh

--- upmpdcli/rootfs/etc/cont-init.d/config.sh
+++ upmpdcli/rootfs/etc/cont-init.d/config.sh
@@ -18,5 +18,5 @@ mkdir -p /data/database \
 #setup configs
 bashio::log.info "Configuring upmpdcli..."
 
-sed "/#friendlyname/c friendlyname = $(bashio::config 'friendlyname')" /etc/upmpdcli.conf
-sed "/#ohproductroom/c ohproductroom = $(bashio::config 'ohproductroom')" /etc/upmpdcli.conf
+sed -i "/#friendlyname/c friendlyname = $(bashio::config 'friendlyname')" /etc/upmpdcli.conf
+sed -i "/#ohproductroom/c ohproductroom = $(bashio::config 'ohproductroom')" /etc/upmpdcli.conf
